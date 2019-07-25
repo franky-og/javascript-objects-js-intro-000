f3b78ffe53808c7488f8abb7034ebcbe0900d64d
Franky Liu <cuifranky95@gmail.com>
2019-07-25 02:03:39 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = {
   artistName: "song",
 }
 function updatePlaylist(playlist, artistName, song) {
-  playlist.artistName = song;
+  playlist[artistName] = song;
   return playlist;
 }
 function removeFromPlaylist(playlist, artistName) {
